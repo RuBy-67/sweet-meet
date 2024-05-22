948bf3defd9dd2c5fc67e9591fb351d4c24f1e45
RuBy-67 <beyer.ruben67@gmail.com>
2024-05-22 21:13:57 +0200

    MemberAdd/Remove

--- src/db.js
+++ src/db.js
@@ -1,5 +1,11 @@
 const mysql = require("mysql2");
-const { db_host, db_user, db_password, db } = require("./jsons/config.json");
+const {
+  db_host,
+  db_user,
+  db_password,
+  db,
+  db_bo,
+} = require("./jsons/config.json");
 
 const connection = mysql.createConnection({
   host: db_host,
@@ -8,9 +14,21 @@ const connection = mysql.createConnection({
   database: db,
 });
 
+const connectionBo = mysql.createConnection({
+  host: db_host,
+  user: db_user,
+  password: db_password,
+  database: db_bo,
+});
+
 connection.connect((err) => {
   if (err) throw err;
   console.log("Connected to the database: " + db);
 });
 
-module.exports = connection;
+connectionBo.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to the BO database: " + db_bo);
+});
+
+module.exports = { connection, connectionBo };
--- src/sql/bddV1.sql
+++ src/sql/bddV1.sql
@@ -1,46 +1,87 @@
-CREATE TABLE IF NOT EXISTS `User` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `discordId` VARCHAR(255) NOT NULL,
-    `power` INT(11) NOT NULL DEFAULT 0,
-    `winCounter` INT(11) NOT NULL DEFAULT 0,
-    `loseCounter` INT(11) NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`)
-);
+CREATE TABLE IF NOT EXISTS mariage (
+    Id int(11) NOT NULL AUTO_INCREMENT,
+    userId varchar(255) NOT NULL,
+    userId2 varchar(255) NOT NULL,
+    Date date NOT NULL,
+    PRIMARY KEY (Id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-CREATE TABLE IF NOT EXISTS `Materiau` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `nom` VARCHAR(255) NOT NULL,
-    `cout` INT(11) NOT NULL,
-    PRIMARY KEY (`id`)
-);
+CREATE TABLE IF NOT EXISTS user (
+    discordId varchar(255) NOT NULL,
+    power int(11) NOT NULL,
+    winCounter int(11) NOT NULL,
+    loseCounter int(11) NOT NULL,
+    PRIMARY KEY (discordId)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-CREATE TABLE IF NOT EXISTS `Possession` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `IdUser` INT(11) NOT NULL,
-    `IdMateriau` INT(11) NOT NULL,
-    `lvl` INT(11) NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`IdUser`) REFERENCES `User`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`IdMateriau`) REFERENCES `Materiau`(`id`) ON DELETE CASCADE
-);
+CREATE TABLE IF NOT EXISTS materiau (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    nom varchar(255) NOT NULL,
+    coût int(11) NOT NULL,
+    type varchar(255) NOT NULL,
+    bonus int(11) NOT NULL,
+    malus int(11) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-CREATE TABLE IF NOT EXISTS `Mariage` (
-    `Id` INT(11) NOT NULL AUTO_INCREMENT,
-    `userId` INT(11) NOT NULL,
-    `userId2` INT(11) NOT NULL,
-    `Date` DATETIME NOT NULL,
-    PRIMARY KEY (`Id`),
-    FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`userId2`) REFERENCES `User`(`id`) ON DELETE CASCADE
-);
+CREATE TABLE IF NOT EXISTS materiau_user (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    idUser varchar(255) NOT NULL,
+    idMateriau int(11) NOT NULL,
+    lvl int(11) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-CREATE TABLE IF NOT EXISTS `Duel` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `userId1` INT(11) NOT NULL,
-    `userId2` INT(11) NOT NULL,
-    `date` DATETIME NOT NULL,
-    `win` INT(11) NOT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`userId1`) REFERENCES `User`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`userId2`) REFERENCES `User`(`id`) ON DELETE CASCADE
-);
+CREATE TABLE IF NOT EXISTS duel (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    userId1 varchar(255) NOT NULL,
+    userId2 varchar(255) NOT NULL,
+    date datetime NOT NULL,
+    win int(11) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS badge (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    nom varchar(255) NOT NULL,
+    type varchar(255) NOT NULL,
+    emojiId varchar(255) NOT NULL,
+    bonus int(11) NOT NULL,
+    malus int(11) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS badge_user (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    idBadge int(11) NOT NULL,
+    idUser varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+ALTER TABLE
+    mariage
+ADD
+    CONSTRAINT mariage_ibfk_1 FOREIGN KEY (userId) REFERENCES user (discordId),
+ADD
+    CONSTRAINT mariage_ibfk_2 FOREIGN KEY (userId2) REFERENCES user (discordId);
+
+ALTER TABLE
+    materiau_user
+ADD
+    CONSTRAINT materiau_user_ibfk_1 FOREIGN KEY (idUser) REFERENCES user (discordId),
+ADD
+    CONSTRAINT materiau_user_ibfk_2 FOREIGN KEY (idMateriau) REFERENCES materiau (id);
+
+ALTER TABLE
+    duel
+ADD
+    CONSTRAINT duel_ibfk_1 FOREIGN KEY (userId1) REFERENCES user (discordId),
+ADD
+    CONSTRAINT duel_ibfk_2 FOREIGN KEY (userId2) REFERENCES user (discordId);
+
+ALTER TABLE
+    badge_user
+ADD
+    CONSTRAINT badge_user_ibfk_1 FOREIGN KEY (idBadge) REFERENCES badge (id),
+ADD
+    CONSTRAINT badge_user_ibfk_2 FOREIGN KEY (idUser) REFERENCES user (discordId);
