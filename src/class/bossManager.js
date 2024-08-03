6c9b05455dde649aca1313be407c8c2118defe63
RuBy-67 <beyer.ruben67@gmail.com>
2024-08-04 01:12:23 +0200

    maj

--- src/class/bossManager.js
+++ src/class/bossManager.js
@@ -39,13 +39,11 @@ class Boss extends DatabaseManager {
     let defense = bossInfo.defense;
     let sante = bossInfo.sante;
     const multiplicateur = Math.random() * (15 - 1.5) + 1.5;
-    let power = Math.round(
-      (bossInfo.attaque + bossInfo.defense + bossInfo.sante) * multiplicateur
-    ); // inconue du joueurs
+
     if (difficulty === "1") {
-      attaque = attaque * 0.5;
-      defense = defense * 0.5;
-      sante = sante * 0.5;
+      attaque = attaque * 0.3;
+      defense = defense * 0.3;
+      sante = sante * 0.3;
       power = power * 0.5;
     } else if (difficulty === "2") {
       attaque = attaque * 0.75;
@@ -53,16 +51,17 @@ class Boss extends DatabaseManager {
       sante = sante * 0.75;
       power = power * 0.75;
     } else if (difficulty === "3") {
-      attaque = attaque * 1.25;
-      defense = defense * 1.25;
-      sante = sante * 1.25;
+      attaque = attaque * 1.55;
+      defense = defense * 1.55;
+      sante = sante * 1.55;
       power = power * 1.25;
     } else if (difficulty === "4") {
-      attaque = attaque * 1.5;
-      defense = defense * 1.5;
-      sante = sante * 1.5;
+      attaque = attaque * 2.5;
+      defense = defense * 2.5;
+      sante = sante * 2.22;
       power = power * 1.5;
     }
+    let power = Math.round((attaque + defense + sante) * multiplicateur); // inconue du joueurs
     const playerScore = await this.getFightScore(
       playerStats.attaque,
       playerStats.defense,
@@ -70,12 +69,8 @@ class Boss extends DatabaseManager {
       playerStats.power
     );
     let bossScore = await this.getFightScore(attaque, defense, sante, power);
-    bossScore = bossScore * 0.18;
-    console.log("player " + playerScore);
-    console.log("boss " + bossScore);
-
+    bossScore = bossScore * 0.22;
     const winner = await this.calculateWinner(playerScore, bossScore);
-    console.log("premier" + winner);
     await this.bossDialogue(
       userId,
       i,
