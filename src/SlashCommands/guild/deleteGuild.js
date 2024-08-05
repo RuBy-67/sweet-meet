c7b59d9dd5d8102dbe7ba6c5665483b97dea9c30
RuBy-67 <beyer.ruben67@gmail.com>
2024-08-05 21:16:45 +0200

    confirm_delete guild

--- src/SlashCommands/guild/deleteGuild.js
+++ src/SlashCommands/guild/deleteGuild.js
@@ -1,4 +1,9 @@
-const { EmbedBuilder } = require("discord.js");
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
 const color = require(`../../jsons/color.json`);
 const config = require("../../jsons/config.json");
 const DatabaseManager = require("../../class/dbManager");
@@ -12,7 +17,7 @@ module.exports = {
     const userId = interaction.user.id;
     const colors = await dbManager.getColor(userId);
 
-    if (config.maintenance) {
+    /*if (config.maintenance) {
       const embed = new EmbedBuilder()
         .setTitle("⚒️ Maintenance ⚒️")
         .setColor(color.error)
@@ -20,7 +25,7 @@ module.exports = {
           `> Le bot est actuellement en maintenance, veuillez réessayer plus tard.`
         );
       return interaction.reply({ embeds: [embed] });
-    }
+    }*/
 
     // Check si l'user fait partie d'une guild
     const guild = await dbManager.getGuildByOwnerId(userId);
@@ -34,21 +39,102 @@ module.exports = {
           text: `Demandé(e) par ${interaction.user.tag}`,
           iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
         });
-      return interaction.reply({ embeds: [embed] });
+      return interaction.reply({ embeds: [embed], ephemeral: true });
     }
 
     // Sinon suprimmer la guilde (et associé)
-    await dbManager.deleteGuildByOwnerId(userId);
+    const confirmRow = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("confirm_delete")
+        .setLabel("✅ Confirmer")
+        .setStyle(ButtonStyle.Danger),
+      new ButtonBuilder()
+        .setCustomId("cancel_delete")
+        .setLabel("❌ Annuler")
+        .setStyle(ButtonStyle.Secondary)
+    );
 
-    const embed = new EmbedBuilder()
-      .setTitle("Guilde supprimée")
+    // Envoyer le message de confirmation
+    const confirmEmbed = new EmbedBuilder()
+      .setTitle("Confirmation de Suppression")
       .setColor(colors)
-      .setDescription(`Votre guilde a été supprimée avec succès ! (triste)`)
+      .setDescription(
+        "Êtes-vous sûr de vouloir supprimer votre guilde ? Cette action est irréversible, et vos fragments investis perdu."
+      )
       .setFooter({
         text: `Demandé(e) par ${interaction.user.tag}`,
         iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
       });
 
-    return interaction.reply({ embeds: [embed] });
+    await interaction.reply({
+      embeds: [confirmEmbed],
+      components: [confirmRow],
+      ephemeral: true,
+      fetchReply: true,
+    });
+
+    // Créer un collecteur pour les boutons
+    const filter = (i) => i.user.id === interaction.user.id;
+    const collector = interaction.channel.createMessageComponentCollector({
+      filter,
+      time: 60000, // 1 minute
+    });
+
+    collector.on("collect", async (i) => {
+      if (i.customId === "confirm_delete") {
+        // Supprimer la guilde si l'utilisateur confirme
+        await dbManager.deleteGuildByOwnerId(userId);
+
+        const successEmbed = new EmbedBuilder()
+          .setTitle("Guilde supprimée")
+          .setColor(colors)
+          .setDescription(
+            "Votre guilde a été supprimée avec succès ! (Vos sujets pleurent cette perte)"
+          )
+          .setFooter({
+            text: `Demandé(e) par ${interaction.user.tag}`,
+            iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
+          });
+
+        await i.update({
+          embeds: [successEmbed],
+          components: [],
+        });
+      } else if (i.customId === "cancel_delete") {
+        // Informer l'utilisateur que la suppression a été annulée
+        const cancelEmbed = new EmbedBuilder()
+          .setTitle("Suppression Annulée")
+          .setColor(colors)
+          .setDescription("La suppression de la guilde a été annulée.")
+          .setFooter({
+            text: `Demandé(e) par ${interaction.user.tag}`,
+            iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
+          });
+
+        await i.update({
+          embeds: [cancelEmbed],
+          components: [],
+        });
+      }
+    });
+
+    collector.on("end", async (collected) => {
+      if (collected.size === 0) {
+        // Informer l'utilisateur que la demande a expiré
+        const timeoutEmbed = new EmbedBuilder()
+          .setTitle("Temps Écoulé")
+          .setColor(color.error)
+          .setDescription("La demande de suppression de la guilde a expiré.")
+          .setFooter({
+            text: `Demandé(e) par ${interaction.user.tag}`,
+            iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
+          });
+
+        await interaction.editReply({
+          embeds: [timeoutEmbed],
+          components: [],
+        });
+      }
+    });
   },
 };
