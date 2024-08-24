4595e8ab3e02f327c9ee5856f20824956f2fb7ad
TatsuyaSapphire <theo.cermak89@gmail.com>
2024-08-24 23:25:14 +0200

    createProfil & showprofil

--- src/SlashCommands/User/createDateprofil.js
+++ src/SlashCommands/User/createDateprofil.js
@@ -1,42 +1,68 @@
-const {
-  ActionRowBuilder,
-  Events,
-  ModalBuilder,
-  TextInputBuilder,
-  TextInputStyle,
-  EmbedBuilder,
-} = require("discord.js");
+const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, InteractionType } = require('discord.js');
 const Dating = require("../../class/dateManager");
-const dateManager = new Dating();
+const db = new Dating();
 
 module.exports = {
-  name: "createdateprofil",
-  description: "créer un profil de date",
-  options: null,
-  run: async (client, interaction, args) => {
-    if (!config.maintenance) {
-      const embed = new EmbedBuilder()
-        .setTitle("⚒️ Maintenance ⚒️")
-        .setColor(color.error)
-        .setDescription(
-          `> La commande est actuellement en maintenance, veuillez réessayer plus tard.`
-        )
-        .setColor(color.error);
-      return interaction.reply({ embeds: [embed] });
-    }
-    if (cooldownInfo) return;
-    function emoji(id) {
-      return (
-        client.emojis.cache.find((emoji) => emoji.id === id)?.toString() ||
-        "Missing Emoji"
-      );
-    }
-    const userId = interaction.user.id;
-    /*const embed = new EmbedBuilder()
-      .setTitle("Titre") ....*/
+    name: 'createdateprofil',
+    description: 'Crée un profil pour date',
 
-    /* pour appeller une fonction de la classe dateManager*/
-    //Exemple :  await dateManager.insertIntoProfile(userId, dateOrientation, dateDesc, dateMP, prenom, age);
-    /* étant des fonction async, il faut les appeler avec await !*/
-  },
+    run: async (interaction) => {
+
+        if (interaction.isCommand() && interaction.commandName ==='createdateprofil') {
+            const modal = new ModalBuilder()
+                .setCustomId('DateModal')
+                .setTitle('Créer ton Profil');
+
+            const prenomInput = new TextInputBuilder()
+                .setCustomId('prenomInput')
+                .setLabel("Quel est votre prénom/pseudo ?")
+                .setStyle(TextInputStyle.Short);
+
+            const ageInput = new TextInputBuilder()
+                .setCustomId('ageInput')
+                .setLabel("Quel âge avez vous ?")
+                .setStyle(TextInputStyle.Short);
+
+            const orientationInput = new TextInputBuilder()
+                .setCustomId('orientationInput')
+                .setLabel("Quel est votre orientation ?")
+                .setStyle(TextInputStyle.Short);
+
+            const descriptionInput = new TextInputBuilder()
+                .setCustomId('descriptionInput')
+                .setLabel("Une petite description de vous?")
+                .setStyle(TextInputStyle.Paragraph);
+
+            const mpInput = new TextInputBuilder()
+                .setCustomId('mpInput')
+                .setLabel("Quel sont vos MP?")
+                .setStyle(TextInputStyle.Short);
+
+            modal.addComponents(
+                new ActionRowBuilder().addComponents(prenomInput),
+                new ActionRowBuilder().addComponents(ageInput),
+                new ActionRowBuilder().addComponents(orientationInput),
+                new ActionRowBuilder().addComponents(descriptionInput),
+                new ActionRowBuilder().addComponents(mpInput)
+            );
+
+            await interaction.showModal(modal);
+        } else if (interaction.type === InteractionType.ModalSubmit) {
+            const user_id = interaction.user.id;
+            const prenom = interaction.fields.getTextInputValue('prenomInput');
+            const age = interaction.fields.getTextInputValue('ageInput');
+            const dateOrientation = interaction.fields.getTextInputValue('orientationInput');
+            const dateDesc = interaction.fields.getTextInputValue('descriptionInput');
+            const dateMP = interaction.fields.getTextInputValue('mpInput');
+
+            await db.insertIntoProfile(
+                user_id,
+                prenom,
+                age,
+                dateOrientation,
+                dateDesc,
+                dateMP
+            );
+        }
+    },
 };
--- src/SlashCommands/User/dateProfil.js
+++ src/SlashCommands/User/dateProfil.js
@@ -1,42 +1,109 @@
-const {
-  ActionRowBuilder,
-  Events,
-  ModalBuilder,
-  TextInputBuilder,
-  TextInputStyle,
-  EmbedBuilder,
-} = require("discord.js");
+const { EmbedBuilder, ButtonBuilder, ButtonStyle, ComponentType, ActionRowBuilder } = require('discord.js');
 const Dating = require("../../class/dateManager");
-const dateManager = new Dating();
+const db = new Dating();
 
 module.exports = {
-  name: "dateprofil",
-  description: "dateprofil",
-  options: null,
-  run: async (client, interaction, args) => {
-    if (!config.maintenance) {
-      const embed = new EmbedBuilder()
-        .setTitle("⚒️ Maintenance ⚒️")
-        .setColor(color.error)
-        .setDescription(
-          `> La commande est actuellement en maintenance, veuillez réessayer plus tard.`
-        )
-        .setColor(color.error);
-      return interaction.reply({ embeds: [embed] });
-    }
-    if (cooldownInfo) return;
-    function emoji(id) {
-      return (
-        client.emojis.cache.find((emoji) => emoji.id === id)?.toString() ||
-        "Missing Emoji"
-      );
-    }
-    const userId = interaction.user.id;
-    /*const embed = new EmbedBuilder()
-      .setTitle("Titre") ....*/
+    name: 'dateprofil',
+    description: 'Affiche les profils de date',
 
-    /* pour appeller une fonction de la classe dateManager*/
-    //Exemple :  await dateManager.insertIntoProfile(userId, dateOrientation, dateDesc, dateMP, prenom, age);
-    /* étant des fonction async, il faut les appeler avec await !*/
-  },
+    run: async (client, message, args) => {
+        if (interaction.isCommand()) {
+            const { commandName } = interaction;
+
+            if (commandName === 'dateprofil') {
+                const profiles = await db.getAllDateProfil( rows => {
+                    if (!profiles) {
+                        return;
+                    }
+
+                    if (rows.length === 0) {
+                        return interaction.reply('Aucun profil n\'a été trouvé.');
+                    }
+
+                    let page = 0;
+
+                    const generateEmbed = async (page) => {
+                        const profil = rows[page];
+                        const targetUser = await client.users.fetch(profil.user_id);
+
+                        return new EmbedBuilder()
+                            .setTitle(`Profil de ${profil.prenomInput}`)
+                            .setDescription(profil.dateDesc)
+                            .setThumbnail(targetUser.displayAvatarURL({ dynamic: true }))
+                            .addFields(
+                                { name: 'Âge', value: `${profil.ageInput}`, inline: true },
+                                { name: 'Orientation', value: `${profil.dateOrientation}`, inline: true },
+                                { name: 'MP', value: `${profil.dateMP}`, inline: true },
+                            )
+                            .setFooter({ text: `Page ${page + 1} sur ${rows.length}` });
+                    };
+
+                    const generateActionRow = () => {
+                        return new ActionRowBuilder().addComponents(
+                            new ButtonBuilder()
+                                .setCustomId('previous')
+                                .setLabel("⬅️")
+                                .setStyle(ButtonStyle.Primary),
+                            new ButtonBuilder()
+                                .setCustomId('like')
+                                .setLabel("❤️")
+                                .setStyle(ButtonStyle.Success),
+                            new ButtonBuilder()
+                                .setCustomId('next')
+                                .setLabel("➡️")
+                                .setStyle(ButtonStyle.Primary)
+                        );
+                    };
+
+                    const embedMessage = interaction.reply({
+                        embeds: [generateEmbed(page)],
+                        components: [generateActionRow()],
+                        fetchReply: true
+                    });
+
+                    const collector = embedMessage.createMessageComponentCollector({
+                        componentType: ComponentType.Button,
+                        time: 60000 
+                    });
+
+                    collector.on('collect', async (i) => {
+                        if (i.customId === 'like') {
+                                const likedId = rows[page].user_id;
+                                const likerId = i.user.id;
+                                const insertLike = await db.insertIntoLIke(likerId, likedId, function (err) {
+                                if (!insertLike) {
+                                    if (!insertLike.includes('SQLITE_CONSTRAINT')) {
+                                        return i.reply({ content: 'Vous avez déjà aimé ce profil.', ephemeral: true });
+                                    }
+                                    return;
+                                }
+                                db.getLikerId(likedId, likerId, (err, row) => {
+                                    if (err) {
+                                        return;
+                                    }
+                                    if (row) {
+                                        const liker = client.users.fetch(likerId);
+                                        const liked = client.users.fetch(likedId);
+                                        liker.send(`Toi et ${liked.username} avez matché! 💓`);
+                                        liked.send(`Toi et ${liker.username} avez matché! 💓`);
+                                    }
+                                    i.reply({ content: 'Vous avez aimé ce profil!', ephemeral: true });
+                                });
+                            });
+                        } else if (i.customId === 'previous') {
+                            page = page > 0 ? --page : rows.length - 1;
+                            i.update({ embeds: [await generateEmbed(page)], components: [generateActionRow()] });
+                        } else if (i.customId === 'next') {
+                            page = page + 1 < rows.length ? ++page : 0;
+                            i.update({ embeds: [await generateEmbed(page)], components: [generateActionRow()] });
+                        }
+                    });
+
+                    collector.on('end', collected => {
+                        embedMessage.edit({ components: [] });
+                    });
+                });
+            }
+        }
+    },
 };
